855edc68874f0930cacba92f791d92972842eacc
Joe Clarke <jclarke@cisco.com>
2021-03-02 12:26:45 -0500

    Initial commit

---
+++ hankify-pw.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import argparse
+import random
+import os
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser(prog=sys.argv[0], description="Generate a Hankified password")
+    parser.add_argument(
+        "--word-file",
+        "-f",
+        metavar="<FILENAME>",
+        help="Path to a list of words",
+        default="./words.txt",
+        required=False,
+    )
+    parser.add_argument(
+        "--num-words",
+        "-n",
+        metavar="<NUMBER OF WORDS>",
+        default=4,
+        type=int,
+        help="Number of words in a password (default: 4)",
+        required=False,
+    )
+    args = parser.parse_args()
+
+    pw = []
+
+    if not os.path.isfile(args.word_file):
+        print(f"ERROR: {args.word_file} does not exist or is not a file")
+        exit(1)
+
+    if args.num_words < 3 or args.num_words > 6:
+        print("ERROR: --num-words must be between 3 and 6")
+        exit(1)
+
+    with open(args.word_file, "r") as fd:
+        contents = fd.read()
+
+    words = contents.split("\n")
+
+    for i in range(args.num_words):
+        while True:
+            word = random.choice(words)
+            if len(word) > 2 and len(word) < 10 and word not in pw:
+                pw.append(word.capitalize())
+                break
+
+    print(f"{''.join(pw)}{random.randint(-1, 100):02}")
+
+
+if __name__ == "__main__":
+    main()
